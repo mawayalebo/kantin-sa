6415637fbbac09b980f8b283e26df154a5f202b1
lebogang mawaya <48476224+mawayalebo@users.noreply.github.com>
2021-07-16 15:03:18 +0200

    header left

--- comps/Header.js
+++ comps/Header.js
@@ -1,13 +1,41 @@
 import styled from "styled-components";
-
+import { Menu } from "@material-ui/icons";
 const Header = () => {
-    return ( 
-        <div>
-            
-        </div>
-     );
-}
+  return (
+    <div>
+      <HeaderContainer>
+        <HeaderLeft>
+          <Menu />
+          <h4>Kantin</h4>
+        </HeaderLeft>
+        <HeaderRight></HeaderRight>
+      </HeaderContainer>
+    </div>
+  );
+};
 
-
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+  background-color: rgb(13, 13, 13);
+  padding: 10px;
+  padding-right: 30px;
+  border-radius: 0px 400px 400px 0px;
+  color: white;
+  margin-top: 10px;
+  > h4 {
+    margin: 0;
+  }
+  > svg {
+    margin-right: 10px;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderRight = styled.div``;
 
 export default Header;
