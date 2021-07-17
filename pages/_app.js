1a93616441b8af8fc6cbb4e189b29e2c4e93731a
Lebogang Mawaya <mawayalebo@gmail.com>
2021-07-16 19:10:03 -0700

    removing materialize

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,6 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return(
     <div>
-      <Head>
-        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/> 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
